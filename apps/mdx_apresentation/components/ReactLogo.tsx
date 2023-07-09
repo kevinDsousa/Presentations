532949b4ea9715551c94136a610cd2a4a55fbb95
kevin <kevindiegodasilvasousa@gmail.com>
2023-07-09 14:18:28 -0300

    fix: Corrigindo as referencias de mdx presentations

--- apps/mdx_apresentation/components/ReactLogo.tsx
+++ apps/mdx_apresentation/components/ReactLogo.tsx
@@ -7,14 +7,13 @@ const style = {
 
 interface ReactProps {
   size: number,
-  src: string,
 }
 
-export function ReactLogo({ size, src }: ReactProps) {
+export function ReactLogo({ size }: ReactProps) {
   return (
     <div>
       <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-        <img width={size} height={size} className={style.logo} alt="React logo" src="https://raw.githubusercontent.com/kevinDsousa/Presentations/262924b76182cb657e8da8f79599ec70badc4d0b/mdx_apresentation/images/react.svg" />
+        <img width={size} height={size} className={style.logo} alt="React logo" src="https://seeklogo.com/images/R/react-logo-7B3CE81517-seeklogo.com.png" />
       </a>
     </div>
   );
